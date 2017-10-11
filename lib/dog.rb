0eb7bcaa38f2c1dff0b524fcf91d4682cc01a8db
github username <github email address>
2017-10-11 12:02:04 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+
+  # def initialize(name)
+  #   @name = name
+  # end
+
+  def name
+    @name
+  end
+
+  def name=(dogName)
+    @name = dogName
+  end
+
+
+  # def initialize(breed)
+  #   @breed = breed
+  # end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dogBreed)
+    @breed = dogBreed
+  end
+
+end
